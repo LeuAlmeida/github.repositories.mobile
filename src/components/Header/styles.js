11781906fabac6779e3653d4d09a29a15e1de327
Léu Almeida <leo@webid.net.br>
2019-12-08 19:42:47 -0300

    Create header stylesheets

---
+++ src/components/Header/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const HeaderView = styled.View`
+  height: 50px;
+  align-items: center;
+  justify-content: center;
+  background: transparent;
+`;
+
+export const HeaderTitle = styled.Text`
+  color: #fff;
+  font-size: 24px;
+`;
