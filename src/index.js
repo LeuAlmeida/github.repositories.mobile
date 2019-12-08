dbaf1d9891ecacfdb29ead0d5fe3c5b364665dc2
Léu Almeida <leo@webid.net.br>
2019-12-08 19:24:32 -0300

    Improving statusbar color

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import Routes from './routes';
 export default function App() {
   return (
     <>
-      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <StatusBar barStyle="light-content" backgroundColor="#ee5676" />
       <Routes />
     </>
   );
