16c4983684037a4b6c67fef0e532be2fbaaa0818
Léu Almeida <leo@webid.net.br>
2019-12-07 22:24:00 -0300

    Import Reactotron Configs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import './config/ReactotronConfig';
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
