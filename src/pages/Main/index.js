5b2766192df3443d6626d107c5fb58a3f0c63266
Léu Almeida <leo@webid.net.br>
2019-12-08 19:56:04 -0300

    GitHub api consuming using axios

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Keyboard } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
-
 import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api from '../../services/api';
+
 import Header from '../../components/Header';
 import { Container, Form, Input, SubmitButton } from './styles';
 
@@ -18,8 +20,24 @@ export default class Main extends Component {
     users: [],
   };
 
-  handleAddUser = () => {
-    console.tron.log(this.state.newUser);
+  handleAddUser = async () => {
+    const { users, newUser } = this.state;
+
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    this.setState({
+      users: [...users, data],
+      newUser: '',
+    });
+
+    Keyboard.dismiss();
   };
 
   render() {
