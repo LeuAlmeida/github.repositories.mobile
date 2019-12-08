2baa244daac98d4f2329a296167d37172925a4c9
Léu Almeida <leo@webid.net.br>
2019-12-07 22:25:03 -0300

    Update reactotron config

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,7 +1,7 @@
 import Reactotron from 'reactotron-react-native';
 
 if (__DEV__) {
-  const tron = Reactotron.configure({ host: '189.78.39.189' })
+  const tron = Reactotron.configure()
     .useReactNative()
     .connect();
 
