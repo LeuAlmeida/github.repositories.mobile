23a40eea850c37d9debbc12676df9dfce87d5dd6
Léu Almeida <leo@webid.net.br>
2019-12-08 20:30:22 -0300

    Update user index page

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -3,6 +3,8 @@ import { View } from 'react-native';
 
 // import { Container } from './styles';
 
-export default function User() {
+export default function User(props) {
+  console.tron.log(props);
+
   return <View />;
 }
