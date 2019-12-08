95a62334c69f6c8d5f390b63b4a87dd98dea9faa
Léu Almeida <leo@webid.net.br>
2019-12-08 20:54:00 -0300

    Stars listing

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -2,7 +2,19 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
 
-import { Container, Header, Avatar, Name, Bio } from './styles';
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
 
 export default class User extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -16,6 +28,7 @@ export default class User extends Component {
     }).isRequired,
   };
 
+  // eslint-disable-next-line react/state-in-constructor
   state = {
     stars: [],
   };
@@ -41,6 +54,20 @@ export default class User extends Component {
           <Name>{user.name}</Name>
           <Bio>{user.bio}</Bio>
         </Header>
+
+        <Stars
+          data={stars}
+          keyExtractor={star => String(star.id)}
+          renderItem={({ item }) => (
+            <Starred>
+              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
       </Container>
     );
   }
