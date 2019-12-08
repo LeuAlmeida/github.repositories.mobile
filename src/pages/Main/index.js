8f6d5bdab8c8e8a49d31bae86367abe54e4b8823
Léu Almeida <leo@webid.net.br>
2019-12-08 20:30:08 -0300

    Incrementing PropTypes

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { StyleSheet, Keyboard, ActivityIndicator } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import LinearGradient from 'react-native-linear-gradient';
@@ -73,10 +74,21 @@ export default class Main extends Component {
     Keyboard.dismiss();
   };
 
-  handleNavigate = () => {
+  handleNavigate = user => {
     const { navigation } = this.props;
 
-    navigation.navigate('User');
+    navigation.navigate('User', { user });
+  };
+
+  // eslint-disable-next-line react/no-typos
+  static PropTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func,
+    }).isRequired,
+  };
+
+  static navigationOptions = {
+    title: 'Usuários',
   };
 
   render() {
@@ -118,7 +130,7 @@ export default class Main extends Component {
                 <Name>{item.name}</Name>
                 <Bio>{item.bio}</Bio>
 
-                <ProfileButton onPress={this.handleNavigate}>
+                <ProfileButton onPress={() => this.handleNavigate(item)}>
                   <ProfileButtonText>Ver Perfil</ProfileButtonText>
                 </ProfileButton>
               </User>
@@ -129,7 +141,3 @@ export default class Main extends Component {
     );
   }
 }
-
-Main.navigationOptions = {
-  title: 'Usuários',
-};
