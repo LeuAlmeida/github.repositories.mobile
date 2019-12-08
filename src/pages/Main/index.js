68567a80ba7cc2cf6fe39ec0470bb712fa0bd557
Léu Almeida <leo@webid.net.br>
2019-12-08 19:15:49 -0300

    Incrementing pink background with react native linear gradient

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,23 +1,38 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Container, Form, Input, SubmitButton } from './styles';
 
+const styles = StyleSheet.create({
+  linearGradient: {
+    flex: 1,
+  },
+});
+
 export default function Main() {
   return (
-    <Container>
-      <Form>
-        <Input
-          autoCorrect={false}
-          autoCapitalize="none"
-          placeholder="Adicionar usuário"
-        />
-        <SubmitButton>
-          <Icon name="add" size={20} color="#FFF" />
-        </SubmitButton>
-      </Form>
-    </Container>
+    <LinearGradient
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+      colors={['#f15b84', '#ee5676', '#e54649']}
+      style={styles.linearGradient}
+    >
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar usuário"
+          />
+          <SubmitButton>
+            <Icon name="add" size={20} color="#FFF" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    </LinearGradient>
   );
 }
 
